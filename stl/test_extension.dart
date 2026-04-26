63c51797edda6567d39f87688e93c64dceb9128e
JedizLaPulga <86472254+JedizLaPulga@users.noreply.github.com>
2026-04-26 16:25:45 +0100

    Remove stl/test_extension.dart
    Delete the obsolete stl/test_extension.dart test file which defined a const extension type I8 and a simple main() print. The test and its I8 type are no longer needed for the current progress of this project.

--- stl/test_extension.dart
+++
@@ -1,9 +0,0 @@
-extension type const I8._(int value) implements int {
-  /// Instantiates a new [I8] spanning a strictly bounded 8-bit value.
-  const I8(this.value);
-}
-
-void main() {
-  const x = I8(5);
-  print(x);
-}
